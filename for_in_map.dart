a017672b51ff5919a782481f77e767cfc1faa7a0
Narotamkumar1 <107026490+Narotamkumar1@users.noreply.github.com>
2022-06-26 15:24:34 +0530

    Create for_in_map.dart

---
+++ for_in_map.dart
@@ -0,0 +1,12 @@
+main() {
+  var numMap = Map();
+  numMap["one"] = 1;
+  numMap["two"] = 2;
+  numMap["three"] = 3;
+  numMap["four"] = 4;
+  numMap["five"] = 5;
+
+  for (var k in numMap.keys) {
+    print("Key : $k, value : ${numMap[k]}");
+  }
+}
