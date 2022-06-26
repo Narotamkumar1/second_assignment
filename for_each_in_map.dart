4b89f2c351a5475cf3acb78201e3df942d103087
Narotamkumar1 <107026490+Narotamkumar1@users.noreply.github.com>
2022-06-26 15:23:14 +0530

    Create for_each_in_map.dart

---
+++ for_each_in_map.dart
@@ -0,0 +1,10 @@
+main() {
+  var numMap = Map();
+  numMap["one"] = 1;
+  numMap["two"] = 2;
+  numMap["three"] = 3;
+  numMap["four"] = 4;
+  numMap["five"] = 5;
+
+  numMap.forEach((k, v) => print("Key : $k, Value : $v"));
+}
